dd50e3bc5eba779d3a56781d08332a659371f2ff
jared <114547220+goldjared@users.noreply.github.com>
2023-06-24 15:54:51 -0400

    change knight.move to output array of possible moves for position

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-/*
-i have a chess board, a knight
-
-i wanted to calculate the shortest path of the knight from point a to point b.
-
-eg knightmove(pointA, pointB) -> [a] . . . [b]
-*/
-
-function board(row, boardArray = []) {
+function board(row = 0, boardArray = []) {
   if (row === 8) {
     return boardArray;
   }
@@ -17,70 +9,9 @@ function board(row, boardArray = []) {
 }
 
 function knight() {
-  let move = (currentPos, targetPos) => {
-    // console.log(targetPos, 'HERE HERE HERE');
-    const screenArray = currentPos.concat(targetPos);
-    let screenArrayResult;
-    screenArray.forEach((pos) => {
-      if (pos < 0 || pos > 7) {
-        screenArrayResult = false;
-      }
-    });
-    if (screenArrayResult === false) return false;
-
-    let colValue = 0;
-    let rowValue = 0;
-    if (currentPos[0] < targetPos[0]) {
-      colValue = targetPos[0] - currentPos[0];
-    } else {
-      colValue = currentPos[0] - targetPos[0];
-    }
-    if (currentPos[1] < targetPos[1]) {
-      rowValue = targetPos[1] - currentPos[1];
-    } else {
-      rowValue = currentPos[1] - targetPos[1];
-    }
-    // console.log(colValue, rowValue, "col, row");
-    if (
-      colValue < 1 ||
-      colValue > 2 ||
-      rowValue < 1 ||
-      rowValue > 2 ||
-      colValue === rowValue
-    ) {
-      return false;
-    } else {
-      currentPos = targetPos;
-      return true;
-    }
-  };
-
-  return { move };
-}
-
-function makeNode(position) {
-  let data = position;
-  let next = [];
-
-  return { data, next };
-}
-
-function graph(currentPos, node) {
-  let visitedNodes = [];
-  let root = buildGraph(currentPos, node);
-
-  function buildGraph(currentPos, node) {
-    if (visitedNodes.length === 64) {
-      console.log("base met.");
-      return;
-    }
-
-    if (visitedNodes.length === 0) {
-      node = makeNode(currentPos);
-
-      visitedNodes.push(node.data);
-    }
-    let possibleMoves = [
+  let move = (currentMove) => {
+    let possibleMoves = [];
+    let moveCombos = [
       [1, 2],
       [1, -2],
       [-1, 2],
@@ -91,43 +22,24 @@ function graph(currentPos, node) {
       [-2, -1],
     ];
 
-    possibleMoves.forEach((moves) => {
-      let currentPossibleMove = [
-        node.data[0] + moves[0],
-        node.data[1] + moves[1],
-      ];
-
-      if (myKnight.move(node.data, currentPossibleMove) != false) {
-        //checking if node (just made, can go there)
-        //check visitedNodes for currentPoss, if it's there, return.
-        // if its not there, node.next.push(makeNode(currentPoss))
-        // and visitedNodes.push(currentPost)
-        if (
-          visitedNodes.find((element) => element === currentPossibleMove) ===
-          currentPossibleMove
-        ) {
-          return;
-        } else {
-          let newNode = makeNode(currentPossibleMove);
-          node.next.push(newNode);
-          visitedNodes.push(currentPossibleMove);
-          // buildGraph(currentPossibleMove, newNode)
-        }
+    moveCombos.forEach((move) => {
+      let newMove = [currentMove[0] + move[0], currentMove[1] + move[1]];
+      if (
+        -1 < newMove[0] &&
+        newMove[0] < 8 &&
+        -1 < newMove[1] &&
+        newMove[1] < 8
+      ) {
+        possibleMoves.push(newMove);
       }
     });
-    return node;
-  }
-  return root;
+    return possibleMoves;
+  };
+  return { move };
 }
-
-let myBoard = board(0);
-let visitedNodes = [];
+let myBoard = board();
+let copyBoard = myBoard;
 let myKnight = knight();
-// console.log(myBoard[0])
-// console.log(myBoard.length);
-let fuckThis = graph([4, 4]);
-
-console.log(fuckThis, "HERE HERE HERE");
-console.log(fuckThis.next[2]);
-console.log(fuckThis.next[3]);
-console.log(visitedNodes);
+console.log(myBoard);
+console.log(myBoard.length);
+console.log(myKnight.move([0, 0]));
